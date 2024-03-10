11fccbde3b4595ccefcc1d647bf43a26e93f02fd
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-10 09:27:41 +0000

    Update mamba_simple.py

--- mamba_ssm/modules/mamba_simple.py
+++ mamba_ssm/modules/mamba_simple.py
@@ -12,8 +12,10 @@ from einops import rearrange, repeat
 
 # refer to local mamba_ssm instead of based mamba_ssm.
 import sys
-#sys.path.insert(0, '/path/to/local/mamba-ssm')
+#sys.path.insert(0, '/path/to/local/mamba-ssm') to import mamba-ssm from local directory
 sys.path.insert(0, '/mnt/fast/nobackup/users/nt00601/mamba-finetune/mamba_ssm')
+import mamba_ssm
+
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 
 try:
