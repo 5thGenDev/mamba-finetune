a6f6952c0ac2758acdf5b0cba762cf3910a7ebf1
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-10 09:23:58 +0000

    Update test_selective_scan.py

--- tests/ops/test_selective_scan.py
+++ tests/ops/test_selective_scan.py
@@ -8,6 +8,10 @@ import pytest
 
 from einops import rearrange
 
+# refer to local mamba_ssm instead of based mamba_ssm.
+import sys
+#sys.path.insert(0, '/path/to/local/mamba-ssm')
+sys.path.insert(0, '/mnt/fast/nobackup/users/nt00601/mamba-finetune/mamba_ssm')
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, selective_scan_ref
 from mamba_ssm.ops.selective_scan_interface import mamba_inner_fn, mamba_inner_ref
 
